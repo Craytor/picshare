da5867d9d9ba203f29c1ca1e796fc45dabf8b58f
Tyler Youschak <tyleryouschak@gmail.com>
2015-05-26 21:56:49 -0400

    Styled the image page

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -2,7 +2,10 @@
     <head>
         <title>Picshare</title>
 
-        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+        <link href="//fonts.googleapis.com/css?family=Lato:100,400" rel="stylesheet" type="text/css">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+
 
         <style>
             body {
@@ -17,15 +20,18 @@
             }
 
             .container {
-                text-align: center;
-                
+                padding-top: 4px;
             }
 
-            .navbar {
+            .top {
                 width: inherit;
                 height: 48px;
                 background-color: #fff;
                 color: #000;
+            }
+
+            .top .fa {
+                font-size: 40px;
             }
 
             .content {
@@ -52,12 +58,20 @@
             .quote {
                 font-size: 24px;
             }
+
+            .name {
+                font-size: 20px;
+                padding-top: 6px;
+                font-weight: 400;
+                display: block;
+                max-width: 400px;
+            }
         </style>
     </head>
     <body>
-        <div class="container">
-            <div class="navbar">
-            <p class="float:right;"><strong>{{ $name }}</strong> <a href="{{ "./" . $hash . "/download" }}">Download</a></p>
+        <div class="top">
+            <div class="container">
+                <div class="name pull-left">{{ $name }}</div> <a class="pull-right" href="{{ "./" . $hash . "/download" }}"><i class="fa fa-arrow-circle-down"></i></a>
             </div>
             <div class="content">
                 <img src="data:{{ $mime }};base64,{{ base64_encode($image) }}" />
